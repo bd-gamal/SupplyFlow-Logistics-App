49715b2bccaff7e835fa0fba9e3c62bfb66f5346
Gamal Badie <149597937+bd-gamal@users.noreply.github.com>
2026-03-13 15:30:33 +0000

    added template pages

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./src/main/resources/templates/*.html",
+        "./src/main/resources/static.css//*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
